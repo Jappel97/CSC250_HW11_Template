05ecb309f702ec9b30247122c3d0c7d76edc5df6
Jappel97 <32598921+Jappel97@users.noreply.github.com>
2017-11-07 03:02:04 -0600

    All your base are belong to us. ROFL

--- app/src/main/java/com/example/awesomefat/csc250_hw11_template/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/csc250_hw11_template/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         //lol
+        //Comment from Joshua Appel: all your codebase are belong to us
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
     }
